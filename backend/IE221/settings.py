274d2fcde6b39467abb1ac3986f13069cec9a918
lamanx-uit <22520732@gm.uit.edu.vn>
2025-09-29 00:48:27 +0700

    Remove unused import and clean up URL patterns in settings and urls files

--- backend/IE221/settings.py
+++ backend/IE221/settings.py
@@ -2,7 +2,6 @@ from datetime import timedelta
 import os
 from pathlib import Path
 from decouple import config
-import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
--- backend/IE221/urls.py
+++ backend/IE221/urls.py
@@ -3,14 +3,17 @@ from django.urls import path, include, re_path
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    # path("admin/", admin.site.urls),
+    path("admin/", admin.site.urls),
     
     # Authentication URLs using Djoser
+    # Checkout Djoser base endpoints: https://djoser.readthedocs.io/en/latest/base_endpoints.html
     path("auth/", include("djoser.urls")),
+    
+    # JWT token management endpoints: https://djoser.readthedocs.io/en/latest/jwt_endpoints.html
     path("auth/login/", include("djoser.urls.jwt")),
     
-    path("api/users/", include("apps.users.urls")),
-    path("api/auth/", include("apps.authentication.urls")),
+    # path("api/users/", include("apps.users.urls")),
+    # path("api/auth/", include("apps.authentication.urls")),
 ]
 
 urlpatterns += [re_path(r"^.*", TemplateView.as_view(template_name="index.html"))]
