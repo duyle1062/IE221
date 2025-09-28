9c0ae96c8abd5f11ad9aa18095ac89174b977982
lamanx-uit <22520732@gm.uit.edu.vn>
2025-09-29 00:05:37 +0700

    Initial Authentication feature, missing test

---
+++ backend/apps/authentication/serializers.py
@@ -0,0 +1,20 @@
+from djoser.serializers import UserCreatePasswordRetypeSerializer as DjoserUserCreateSerializer
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class UserCreateSerializer(DjoserUserCreateSerializer):
+    class Meta(DjoserUserCreateSerializer.Meta):
+        model = User
+        fields = ("id", "email", "firstname", "lastname", "password", "gender", "phone")
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Add custom claims
+        token['role'] = user.role
+        return token
