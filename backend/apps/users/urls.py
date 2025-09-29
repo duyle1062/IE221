d373654ebd967029c2ee6846e50d823b59fab30b
ThanhDanh <thanhdanhdo1701@gmail.com>
2025-09-29 22:27:12 +0700

    feature: API Get current user's personal information

--- backend/apps/users/urls.py
+++ backend/apps/users/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import hello
+from .views import get_user_profile, get_current_user_info
+
+app_name = 'users'
 
 urlpatterns = [
-    path("hello/", hello),
+    # User profile endpoints
+    path("profile/", get_user_profile, name='get_user_profile'),
+    path("current-user-info/", get_current_user_info, name='get_current_user_info'),
 ]
